99ec2862b2256fe3fcddd0fa6d5ef6fcadbe779f
Kevin Charles <rossgithub@gmail.com>
2017-06-27 22:46:02 +0100

    Changes to init.el
    Added section to automatically install required packages
    Added some descriptive lines

--- init.el
+++ init.el
@@ -5,12 +5,32 @@
 
 ;; ensure intended version of emacs
 (when (< emacs-major-version 24)
-  for important compatibility libraries like cl-lib
+  ;; for important compatibility libraries like cl-lib
   (add-to-list 'package-archives
   	       '("gnu" . "http://elpa.gnu.org/packages/")))
 
 ;; init packages
 (package-initialize)
+
+(defvar required-packages
+  '(;; Go packages to install
+    go-mode
+    go-eldoc
+    go-autocomplete
+    golint
+
+    ;; Environment packages to install
+    project-explorer)
+  "packages required for this init.el")
+
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package required-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; configure auto complete
 (ac-config-default)
@@ -36,7 +56,7 @@
   (add-hook 'before-save-hook 'gofmt-before-save)
   (local-set-key (kbd "M-.") 'godef-jump))
 
-;; <describe>
+;; run the go-mode-setup function when go mode is activated
 (add-hook 'go-mode-hook 'go-mode-setup)
 
 ;; enable wind move keys (shift + arrow) to move between eindows
@@ -54,4 +74,18 @@
 (add-hook 'find-file-hook 'linum-mode)
 
 ;; turn on 80 column marker limit when a file buffer is opened
-(add-hook 'find-file-hook (lambda () (interactive) (column-marker-1 80)))
+;;(add-hook 'find-file-hook (lambda () (interactive) (column-marker-1 80)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(column-marker project-explorer golint go-eldoc go-autocomplete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
